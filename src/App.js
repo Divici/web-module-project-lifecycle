33081db67ceadf3e708996bfc359d2a9a4138f3d
david-aihe <doa9200@gmail.com>
2022-01-11 16:05:25 -0500

    Built out DOM

--- src/App.js
+++ src/App.js
@@ -2,8 +2,51 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
+    return(<div className='wrapper'>
+      <h1>GITHUB INFO</h1>
+
+      <div className='search'>
+        <form>
+          <input
+            placeholder='Github Handle'
+          >
+          </input>
+          <button>Search</button>
+        </form>
+      </div>
+
+      <div className='flex'>
+        <div className='user'>
+          <img src='' alt='person'></img>
+        </div>
+        <div className='info'>
+          <h2>Charles Linkler</h2>
+          <h3>(Clinkler_Programmer)</h3>
+          <p>Total Repos: 101</p>
+          <p>Total Followers: 29</p>
+        </div>
+      </div>
+
+      <div className='flex followers'>
+        <h2 className='span'>FOLLOWERS:</h2>
+        <div className=' flex follower'>
+          <img src='' alt='follower'></img>
+          <h3>NicholAlexander</h3>
+        </div>
+        <div className=' flex follower'>
+          <img src='' alt='follower'></img>
+          <h3>NicholAlexander</h3>
+        </div>
+        <div className=' flex follower'>
+          <img src='' alt='follower'></img>
+          <h3>NicholAlexander</h3>
+        </div>
+        <div className='flex follower'>
+          <img src='' alt='follower'></img>
+          <h3>NicholAlexander</h3>
+        </div>
+      </div>
+
     </div>);
   }
 }
